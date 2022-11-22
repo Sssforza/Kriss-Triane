e317df3af18f76b99f555de933c8095fe0305f86
Katy <katena.moskvina@bk.ru>
2022-11-22 13:39:24 +0600

    header

--- public_html/local/templates/Kriss-Triane/header.php
+++ public_html/local/templates/Kriss-Triane/header.php
@@ -11,4 +11,31 @@
 
 <body>
     <div class="wrapper">
-        <header class="header"></header>
+        <header class="header">
+            <div class="header__content">
+                <div class="header__block">
+                    <div class="header__address">Омск, ул. Волочаевская, 15/2</div>
+                    <div class="header__working">Ежедневно с 9:00 до 21:00</div>
+                </div>
+                <div class="header__block">
+                    <div class="header__logo">
+                        <?include $_SERVER['DOCUMENT_ROOT'] . '/dist/img/svg/logo.svg';?>
+                    </div>
+                </div>
+                <div class="header__block header__block_right">
+                    <div class="header__wrapper">
+                        <div class="header__tel">
+                            <a href="tel:+7(3812)220011" class="header__link">+7 (3812) 22-00-11</a>
+                        </div>
+                        <div class="header__request">
+                            <a href="#" class="header__call">Заказать звонок</a>
+                        </div>
+                    </div>
+                    <div class="hamburger">
+                        <div class="hamburger__box">
+                            <div class="hamburger__inner"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
