3216c40e8f0def14e5aac8bcb7858e0233e5f4e5
Katy <katena.moskvina@bk.ru>
2022-11-23 21:48:49 +0600

    footer

--- public_html/local/templates/Kriss-Triane/footer.php
+++ public_html/local/templates/Kriss-Triane/footer.php
@@ -1,4 +1,48 @@
-            <footer class="footer"></footer>
+            <footer class="footer">
+                <div class="footer__content">
+                    <div class="footer__box">
+                        <div class="footer__title">Ждем вас <br>в&nbsp;нашей студии</div>
+                        <div class="footer__wrapper">
+                            <div class="footer__wrap">
+                                <div class="footer__address">г. Омск, ул. Волочаевская 15&nbsp;к.2</div>
+                                <div class="footer__reference">
+                                    <a href="#" class="footer__link">Показать&nbsp;на&nbsp;карте</a>
+                                </div>
+                            </div>
+                            <div class="footer__wrap">
+                                <div class="footer__reference">
+                                    <a href="tel:+7(3812)220011" class="footer__tel">+7 (3812) 22-00-11</a>
+                                </div>
+                                <div class="footer__reference">
+                                    <a href="tel:+7(913)6403131" class="footer__tel">+7 (913) 640-3131</a>
+                                </div>
+                            </div>
+                            <div class="footer__wrap">
+                                <div class="footer__reference">
+                                    <a href="#" class="footer__link">Мы&nbsp;ВКонтакте</a>
+                                </div>
+                                <div class="footer__reference">
+                                    <a href="#" class="footer__link">Мы&nbsp;в&nbsp;Telegram</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__box footer__img">
+                        <div class="footer__bg" style="background-image:url(/dist/img/footerImg.jpg)"></div>
+                    </div>
+                    <div class="footer__box footer__box_bottom">
+                        <div class="footer__logo">
+                            <?include $_SERVER['DOCUMENT_ROOT'] . '/dist/img/svg/logo.svg';?>
+                        </div>
+                        <div class="footer__wrap">
+                            <div class="footer__politics">
+                                <a href="#" class="footer__policy">Политика&nbsp;конфиденциальнности</a>
+                            </div>
+                            <div class="footer__KrissTriane">Kriss Triane &copy;&nbsp;2018&nbsp;г. Все права защищены.</div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     </body>
 </html>
